ffb950e7fbdd1e988c0417ecea198160fc0697b0
Alexandre Hermes <133255531+Alexandre200319@users.noreply.github.com>
2024-04-05 20:36:28 -0300

    criação página home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,51 +1,74 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="mb-8">Escolha sua matéria</h1>
+    <div className="flex flex-col bg-[#ffa742] items-center justify-center h-screen font-sans ">
+      <h1 className="mb-4 pt-20 text-3xl  font-bold text-white">
+        Escolha sua matéria
+      </h1>
       <div className="relative flex flex-wrap justify-center w-full h-full">
-        <button className="absolute top-0 left-0">
-          <Image
-            src="/images/materia1.jpg"
-            alt="Matéria 1"
-            width={150}
-            height={150}
-          />
-        </button>
-        <button className="absolute top-0 right-0">
-          <Image
-            src="/images/materia2.jpg"
-            alt="Matéria 2"
-            width={150}
-            height={150}
-          />
-        </button>
-        <button className="absolute bottom-0 right-0">
-          <Image
-            src="/images/materia3.jpg"
-            alt="Matéria 3"
-            width={150}
-            height={150}
-          />
-        </button>
-        <button className="absolute bottom-0 left-0">
-          <Image
-            src="/images/materia4.jpg"
-            alt="Matéria 4"
-            width={150}
-            height={150}
-          />
-        </button>
-        <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Image
-            src="/images/materia5.jpg"
-            alt="Matéria 5"
-            width={150}
-            height={150}
-          />
-        </button>
+        <Link href="/Materias/matematica" passHref>
+          <button className="m-4 p-4 bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300">
+            <Image
+              src="/images/imagem.jpg"
+              alt="Matéria 1"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
+            <span className="block text-[#ffa742] mt-2">Matemática</span>
+          </button>
+        </Link>
+        <Link href="/Materias/ciencias" passHref>
+          <button className="m-4 p-4 bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300">
+            <Image
+              src="/images/imagem.jpg"
+              alt="Matéria 2"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
+            <span className="block text-[#ffa742] mt-2">Ciências</span>
+          </button>
+        </Link>
+        <Link href="/Materias/linguas" passHref>
+          <button className="m-4 p-4 bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300">
+            <Image
+              src="/images/imagem.jpg"
+              alt="Matéria 3"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
+            <span className="block text-[#ffa742] mt-2">Línguas</span>
+          </button>
+        </Link>
+        <Link href="/materias/outros" passHref>
+          <button className="m-4 p-4 bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300">
+            <Image
+              src="/images/imagem.jpg"
+              alt="Matéria 4"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
+            <span className="block text-[#ffa742] mt-2">Outros</span>
+          </button>
+        </Link>
+        <Link href="/materias/quimica" passHref>
+          <button className="m-4 p-4 bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300">
+            <Image
+              src="/images/imagem.jpg"
+              alt="Matéria 5"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
+            <span className="block text-[#ffa742] mt-2">Química</span>
+          </button>
+        </Link>
       </div>
     </div>
   );
